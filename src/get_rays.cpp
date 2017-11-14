3cb56332946a4702adb4a07c31985bd1719d7e09
Fedor Scholz <scholz@fzi.de>
2017-11-14 12:12:07 +0100

    connect to robot in nrp, store items in memory, add comments and cleanup

--- src/get_rays.cpp
+++ src/get_rays.cpp
@@ -6,103 +6,148 @@
 #include "opencv2/features2d/features2d.hpp"
 #include "cv_bridge/cv_bridge.h"
 #include "image_geometry/pinhole_camera_model.h"
-#include "sensor_msgs/JointState.h"
+#include "std_msgs/Float64.h"
 #include "sensor_msgs/CameraInfo.h"
+#include "ros_holographic/NewObject.h"
 
 #include <math.h>
 #include <algorithm>
 #include <sstream>
+#include <iostream>
 
-image_transport::Publisher  pubkps;
-ros::Publisher  pubcmd;
-cv::SimpleBlobDetector detector;
+image_transport::Publisher pubkps;
+cv::Ptr<cv::SimpleBlobDetector> detector;
 image_geometry::PinholeCameraModel model;
 sensor_msgs::CameraInfoConstPtr ptr;
-sensor_msgs::JointState j;
+ros::Publisher pub_neck_yaw_pos;
+ros::Publisher pub_neck_yaw_vel;
+ros::Publisher pub_neck_pitch_pos;
+ros::Publisher pub_neck_pitch_vel;
+std_msgs::Float64 neck_yaw_pos;
+std_msgs::Float64 neck_yaw_vel;
+std_msgs::Float64 neck_pitch_pos;
+std_msgs::Float64 neck_pitch_vel;
 double x = 0, y = 0, thresh = 0.05;
 bool set = false;
 bool block = false;
+ros::ServiceClient memory_client;
+ros_holographic::NewObject obj;
+int counter = 0;
 
-bool compare_size(cv::KeyPoint first, cv::KeyPoint second)
-{
-  return first.size > second.size;
+bool compare_size(cv::KeyPoint first, cv::KeyPoint second) {
+        return first.size > second.size;
 }
 
-void imageCallback2(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& nptr){
-    ptr = nptr;
-    set = true;
+void imageCallback2(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& nptr) {
+        ptr = nptr;
+        set = true;
 }
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msg){
-  cv::Mat mat = cv_bridge::toCvShare(msg, "bgr8")->image;
-  cv::cvtColor(mat,mat, CV_BGR2GRAY );
-  cv::threshold(mat,mat,100,255,0);
-  cv::resize(mat,mat,cv::Size(ptr->width,ptr->height));
-  std::vector<cv::KeyPoint> keypoints;
-  detector.detect(mat, keypoints);
-  cv::Mat kps;
-  cv::drawKeypoints( mat, keypoints, kps, cv::Scalar(0,0,255), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-  std::sort(keypoints.begin(),keypoints.end(),compare_size);
-  cv::cvtColor(kps,kps, CV_BGR2GRAY );
-  sensor_msgs::ImageConstPtr ikps;
-  ikps = cv_bridge::CvImage(std_msgs::Header(), "mono8", kps).toImageMsg();
-  pubkps.publish(ikps);    
-  if (keypoints.size() && set && !block) {
-    block = true;
-    ROS_ERROR_STREAM("" << model.fromCameraInfo(ptr));
-    cv::Point3d p = model.projectPixelTo3dRay(keypoints[0].pt);
-    double jx, jy;
-    jx = atan(- p.x);
-    jy = atan(- p.y / sqrt(p.x * p.x + 1));
-    ROS_ERROR("%f %f",keypoints[0].pt.x, keypoints[0].pt.y);
-    ROS_ERROR("%f %f",jx,jy);
-    if (true/*abs(jx) > thresh || abs(jy) > thresh*/){
-      j.header.stamp = ros::Time::now();
-      x += jx;
-      y += jy;
-      j.position[0] = x;
-      j.position[1] = y;
-      pubcmd.publish(j);
-    }
-    ros::Duration(5).sleep();
-    block = false;
-  }
+void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
+        // convert ros image to cv image
+        cv::Mat mat = cv_bridge::toCvShare(msg, "bgr8")->image;
+        // convert to gray scale image
+        cv::cvtColor(mat, mat, CV_BGR2GRAY);
+        // make too dark pixels black
+        cv::threshold(mat, mat, 100, 255, 0);
+        // resize
+        cv::resize(mat, mat, cv::Size(msg->width, msg->height));
+        // detect keypoints
+        std::vector<cv::KeyPoint> keypoints;
+        detector->detect(mat, keypoints);
+        // make keypoints visible
+        cv::Mat kps;
+        cv::drawKeypoints(mat, keypoints, kps, cv::Scalar(0, 0, 255), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+        std::sort(keypoints.begin(), keypoints.end(), compare_size);
+        // convert to gray scale image
+        cv::cvtColor(kps, kps, CV_BGR2GRAY);
+        // publish image
+        sensor_msgs::ImageConstPtr ikps;
+        ikps = cv_bridge::CvImage(std_msgs::Header(), "mono8", kps).toImageMsg();
+        pubkps.publish(ikps);    
+        if (keypoints.size() && set && !block) {
+                block = true;
+                ROS_ERROR_STREAM("" << model.fromCameraInfo(ptr));
+                cv::Point3d p = model.projectPixelTo3dRay(keypoints[0].pt);
+                double dx, dy;
+                dx = atan(-p.x);
+                dy = atan(-p.y / sqrt(p.x * p.x + 1)); // cartesian to polar coordinates
+                ROS_ERROR("keypoints: %f %f", keypoints[0].pt.x, keypoints[0].pt.y);
+                ROS_ERROR("differences: %f %f", dx, dy);
+//                if (abs(dx) > thresh || abs(dy) > thresh) {
+                        // send new positions
+                        x += dx;
+                        y += dy;
+                        std::cout << "going to look at: " << x << ", " << y << "\n";
+                        neck_yaw_pos.data = x;
+                        neck_pitch_pos.data = y;
+                        pub_neck_yaw_pos.publish(neck_yaw_pos);
+                        pub_neck_pitch_pos.publish(neck_pitch_pos);
+
+                        // send info to memory
+                        obj.request.X = x;
+                        obj.request.Y = y;
+                        std::stringstream sstm;
+                        sstm << "label no " << counter;
+                        obj.request.Label = sstm.str();
+                        std::cout << "going to label label " << counter << " at: " << x << ", " << y << "\n";
+                        counter++;
+                        memory_client.call(obj);
+//                }
+                ros::Duration(5).sleep();
+
+                block = false;
+        }
 }
 
-int main(int argc, char **argv)
-{
-  cv::SimpleBlobDetector::Params params;
-  params.minThreshold = 35;
-  params.maxThreshold = 255;
-  params.minDistBetweenBlobs = 0.0f;
-  params.filterByInertia = false;
-  params.filterByConvexity = false;
-  params.filterByColor = true;
-  params.blobColor = 255;
-  params.filterByCircularity = false;
-  params.filterByArea = true;
-  params.minArea = 50;
-  detector = cv::SimpleBlobDetector(params);
-  j.name.resize(2);
-  j.position.resize(2);
-  j.velocity.resize(2);
-  j.name[0] = "ptu_pan";
-  j.name[1] = "ptu_tilt";
-  j.velocity[0] = 0.25;
-  j.velocity[1] = 0.25;
-  j.position[0] = x;
-  j.position[1] = y;
-  ros::init(argc, argv, "get_ray");
-  ros::NodeHandle nh;
-  image_transport::ImageTransport it(nh);
-  pubkps = it.advertise("/kps_map",1);
-  pubcmd = nh.advertise<sensor_msgs::JointState>("/ptu/cmd", 1); 
-  j.header.stamp = ros::Time::now();
-  pubcmd.publish(j);  
-  ros::Duration(10).sleep();
-  image_transport::Subscriber sub = it.subscribe("/saliency_map", 1, imageCallback);
-  image_transport::CameraSubscriber csub = it.subscribeCamera("/rgb/image_raw", 1, imageCallback2);
-  ros::spin();
-  
-  return 0;
+int main(int argc, char **argv) {
+        // initialize blob detector
+        cv::SimpleBlobDetector::Params params;
+        params.minThreshold = 35;
+        params.maxThreshold = 255;
+        params.minDistBetweenBlobs = 0.0f;
+        params.filterByInertia = false;
+        params.filterByConvexity = false;
+        params.filterByColor = true;
+        params.blobColor = 255;
+        params.filterByCircularity = false;
+        params.filterByArea = true;
+        params.minArea = 50;
+        detector = cv::SimpleBlobDetector::create(params);
+        
+        // initialize neck msgs
+        neck_yaw_pos.data = x;
+        neck_yaw_vel.data = 0.25;
+        neck_pitch_pos.data = y;
+        neck_pitch_vel.data = 0.25;
+        
+        // initialize ros node
+        ros::init(argc, argv, "get_rays");
+        ros::NodeHandle nh;
+        image_transport::ImageTransport it(nh);
+        
+        // be able to publish to /kps_map and robot neck controls
+        pubkps = it.advertise("/kps_map",1);
+        pub_neck_yaw_pos = nh.advertise<std_msgs::Float64>("/robot/neck_yaw/pos", 1); 
+        pub_neck_yaw_vel = nh.advertise<std_msgs::Float64>("/robot/neck_yaw/vel", 1); 
+        pub_neck_pitch_pos = nh.advertise<std_msgs::Float64>("/robot/neck_pitch/pos", 1); 
+        pub_neck_pitch_vel = nh.advertise<std_msgs::Float64>("/robot/neck_pitch/vel", 1); 
+        
+        // publish first states
+        pub_neck_yaw_pos.publish(neck_yaw_pos);
+        pub_neck_yaw_vel.publish(neck_yaw_vel);
+        pub_neck_pitch_pos.publish(neck_pitch_pos);
+        pub_neck_pitch_vel.publish(neck_pitch_vel);
+        ros::Duration(10).sleep();
+        
+        // subscribe and do stuff
+        image_transport::Subscriber sub = it.subscribe("/saliency_map", 1, imageCallback);
+        image_transport::CameraSubscriber csub = it.subscribeCamera("/icub_model/left_eye_camera/image_raw", 1, imageCallback2);
+
+        // initialize service client
+        memory_client = nh.serviceClient<ros_holographic::NewObject>("new_object");
+
+        ros::spin();
+        
+        return 0;
 }
