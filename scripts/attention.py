f277ae77c1bd93ecda15001ee3f00adc986f18da
Fedor Scholz <scholz@fzi.de>
2018-01-08 11:56:11 +0100

    add main node

---
+++ scripts/attention.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+PKG = 'embodied_attention'
+import roslib; roslib.load_manifest(PKG)
+
+import rospy
+from geometry_msgs.msg import Point
+from std_msgs.msg import Float64
+from sensor_msgs.msg import Image, CameraInfo
+from image_geometry import PinholeCameraModel
+from embodied_attention.srv import Roi, Look
+from ros_holographic.srv import NewObject, ProbeLabel
+from cv_bridge import CvBridge, CvBridgeError
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import misc
+
+import thread
+
+class Attention():
+    def __init__(self):
+        saccade_sub = rospy.Subscriber("/saccade_target", Point, self.saccade_callback, queue_size=1, buff_size=2**24)
+        camera_sub = rospy.Subscriber("/icub_model/left_eye_camera/image_raw", Image, self.camera_callback, queue_size=1, buff_size=2**24)
+        camera_info_sub = rospy.Subscriber("/icub_model/left_eye_camera/camera_info", CameraInfo, self.camera_info_callback, queue_size=1, buff_size=2**24)
+        saliency_sub = rospy.Subscriber("/saliency_map", Image, self.saliency_callback, queue_size=1, buff_size=2**24)
+
+        self.pan_pub = rospy.Publisher("/robot/left_eye_pan/pos", Float64, queue_size=1)
+        self.tilt_pub = rospy.Publisher("/robot/eye_tilt/pos", Float64, queue_size=1)
+        self.roi_pub = rospy.Publisher("/roi", Image, queue_size=1)
+
+        self.recognizer = rospy.ServiceProxy('recognize', Roi)
+        self.memory = rospy.ServiceProxy('new_object', NewObject)
+        self.probe = rospy.ServiceProxy('probe_label', ProbeLabel)
+
+        self.looker = rospy.Service('look', Look, self.look)
+
+        self.camera = None
+        self.camera_info = None
+        self.saliency = None
+        self.model = PinholeCameraModel()
+
+        self.x = 0.
+        self.y = 0.
+
+        self.limit = 0.942477796 
+
+    def saccade_callback(self, saccade):
+        if self.camera is not None and self.camera_info is not None and self.saliency is not None:
+            # scale to camera image size
+            x = int(saccade.x * (float(self.camera.width)/self.saliency.width))
+            y = int(saccade.y * (float(self.camera.height)/self.saliency.height))
+
+            # compute target in polar coordinates
+            self.model.fromCameraInfo(self.camera_info)
+            ray = self.model.projectPixelTo3dRay((x, y))
+            dx = np.arctan(-ray[0])
+            dy = np.arctan(-ray[1] / np.sqrt(ray[0] * ray[0] + 1))
+
+            # publish new position
+            if self.x + dx < -self.limit or self.x + dx > self.limit or self.y + dy < -self.limit or self.y + dy > self.limit:
+                print "\tover eye movement limit! dropped!"
+                return
+            self.x += dx
+            self.y += dy
+            self.pan_pub.publish(self.x)
+            self.tilt_pub.publish(self.y)
+
+            # set roi
+            size = 25
+            x1 = self.camera_info.width/2 - size
+            y1 = self.camera_info.height/2 - size
+            x2 = self.camera_info.width/2 + size
+            y2 = self.camera_info.height/2 + size
+
+            image = CvBridge().imgmsg_to_cv2(self.camera, "bgr8")
+            roi = image[y1:y2, x1:x2]
+            roi = CvBridge().cv2_to_imgmsg(roi, "bgr8")
+            self.roi_pub.publish(roi)
+
+            # object recognition
+            label = self.recognizer(roi).Label
+
+            # store in memory
+            self.memory(self.x * 100, self.y * 100, label)
+            print "stored in memory: %d, %d, %s" % (self.x * 100, self.y * 100, label)
+        else:
+            print "but information is missing"
+
+    def camera_callback(self, camera):
+        self.camera = camera
+
+    def camera_info_callback(self, camera_info):
+        self.camera_info = camera_info
+
+    def saliency_callback(self, saliency):
+        self.saliency = saliency
+
+    def look(self, label):
+        # ask memory for label
+        p = self.probe(label.Label)
+
+        if not p.return_value or len(p.X) == 0 or len(p.Y) == 0:
+            return False
+
+        x = p.X[0]
+        y = p.Y[0]
+
+        # adjust camera
+        self.pan_pub.publish(x/100.)
+        self.tilt_pub.publish(y/100.)
+        return True
+
+def main(args):
+    rospy.init_node("attention")
+    attention = Attention()
+    rospy.spin()
+
+if __name__ == '__main__':
+    main(sys.argv)
