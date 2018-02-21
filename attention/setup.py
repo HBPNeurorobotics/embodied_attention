3ecc9d7c5ae440a5a6388c6fe6e43730416c2339
Fedor Scholz <scholz@fzi.de>
2018-02-21 13:27:30 +0100

    refactor: move non-ros-related code into separate package

---
+++ attention/setup.py
@@ -0,0 +1,22 @@
+"""
+Attention package
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name = "attention",
+    version = "0.0.1",
+    description = ("Attention package"),
+    keywords = "attention, saliency, saccade",
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    # Choose your license
+    license='FZI',
+    install_requires=[]
+)
--- scripts/saccade.py
+++ scripts/saccade.py
@@ -3,120 +3,44 @@ PKG = 'embodied_attention'
 import roslib; roslib.load_manifest(PKG)
 
 import rospy
-import cv2 as cv
-from cv_bridge import CvBridge, CvBridgeError
-from std_msgs.msg import Float32MultiArray
-from geometry_msgs.msg import Point
+from attention import Saccade
 from embodied_attention.srv import ResetSaccade
-import sys
-import os
-import numpy as np                                                                                                                                                                                                         
-from scipy import misc
+from std_msgs.msg import Float32MultiArray
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Point
+import numpy as np
 
-## functions
-def gauss(x, y, X, Y, sigma):
-    return np.exp(-(np.power(x-X, 2)+np.power(y-Y, 2))/(2.*np.power(sigma ,2)))
-def f(x): return np.maximum(x, 0.)
-
-class Saccade():
+class SaccadeNode():
     def __init__(self):
-    
-        ## parameters
-        self.N       =  1600       # number of neurons per type (visual, movement)
-        self.theta   =    11.      # decision threshold
-        sig_lat      =      .25    # width of Gaussian lateral inhibition
-        self.sig_IoR =      .05    # width of Gaussian spread of inhibition of return
-        sig_noise    =      .2     # width of Gaussian noise
-        self.k       =      .0175  # passive decay rate (movement neurons)
-        self.g       =      .33    # input threshold
-        self.G       =      .2     # scaling factor for lateral inhibition
-        
-        ## setup
-        # dimensions and coordinate systems
-        self.Ns        = int(np.sqrt(self.N))  
-        n              = 1./(self.Ns*.5)
-        r              = np.linspace(-1., 1., self.Ns)
-        self.X, self.Y = np.meshgrid(r, r)
-        self.X         = np.reshape(self.X, [self.N, ])
-        self.Y         = np.reshape(self.Y, [self.N, ])
-        
-        # lateral weights 
-        self.W        = np.zeros([self.N, self.N])
-        for i in range(self.N):
-            self.W[:, i] = gauss(self.X[i], self.Y[i], self.X, self.Y, sig_lat)
-            self.W[i, i] = 0.
-            
-        #self.dt = .1
-        self.dt = .75
-        self.tau = 20.
-        
-        # noise propagation
-        self.dsig_v  = np.sqrt(self.dt/self.tau)*sig_noise # input (visual neuron) noise
-        self.dsig_m  = np.sqrt(self.dt)*sig_noise          # movement neuron noise
-        
-        # (state) variables
-        self.M               = np.zeros(self.N)            # movement neurons
-        self.V               = np.zeros(self.N)            # visual neurons
-    
+        self.saccade = Saccade()
+
         self.saliency_sub = rospy.Subscriber("/saliency_map", Float32MultiArray, self.saliency_map_callback, queue_size=1, buff_size=2**24)
 
-        self.target_pub = rospy.Publisher("/saccade_target", Point, queue_size=1)
-        self.potential_target_pub = rospy.Publisher("/saccade_potential_target", Point, queue_size=1)
-
-        self.cv_bridge = CvBridge()
+        self.saccade_target_pub = rospy.Publisher("/saccade_target", Point, queue_size=1)
+        self.saccade_potential_target_pub = rospy.Publisher("/saccade_potential_target", Point, queue_size=1)
 
         self.reset_saccade_serv = rospy.Service('/reset_saccade', ResetSaccade, self.handle_reset_saccade)
 
-    # numerical integration (simple Euler)
     def saliency_map_callback(self, saliency_map):
 
-        # handle input
         lo = saliency_map.layout
-        sal = np.asarray(saliency_map.data[lo.data_offset:]).reshape(lo.dim[0].size, lo.dim[1].size)
+        saliency_map = np.asarray(saliency_map.data[lo.data_offset:]).reshape(lo.dim[0].size, lo.dim[1].size)
 
-        sal = misc.imresize(sal, [self.Ns, self.Ns])
-        sal = np.reshape(sal, [self.N, ])/235.*0.55+.2
+        (target, is_actual_target) = self.saccade.compute_saccade_target(saliency_map)
 
-        # update
-        self.V += self.dt*(-self.V + sal)/self.tau + self.dsig_v*np.random.randn(self.N)
-        self.M += self.dt*(-self.k*self.M + f(self.V - self.g) - self.G*np.dot(self.W, f(self.M))) + self.dsig_m*np.random.randn(self.N)
-
-        ID = np.argmax(self.M)
-
-        # transform to coordinates in saliency map
-        x = np.mod(ID, self.Ns) + 0.5
-        y = int(ID/self.Ns) + 0.5
-        x_scaled = int(float(lo.dim[0].size)/self.Ns * x)
-        y_scaled = int(float(lo.dim[1].size)/self.Ns * y)
-        rospy.loginfo("Potential target: %3d, %3d: %f" % (x_scaled, y_scaled, self.M[ID]))
-
-        # puslish potential target
-        self.potential_target_pub.publish(Point(x_scaled, y_scaled, self.M[ID]))
-
-        # check if target
-        if (self.M[ID] >= self.theta):
-            rospy.loginfo("\tis actual target")
-
-            # publish target
-            self.target_pub.publish(Point(x_scaled, y_scaled, self.M[ID]))
-
-            # reset
-            self.M[ID] = 0.
-
-            # inhibition of return
-            self.V = self.V - gauss(self.X[ID], self.Y[ID], self.X, self.Y, self.sig_IoR)
+        self.saccade_potential_target_pub.publish(Point(*target))
+        if is_actual_target:
+            self.saccade_target_pub.publish(Point(*target))
 
     def handle_reset_saccade(self, req):
         rospy.loginfo("Resetting node")
-        self.saliency_sub.unregister()
-        self.reset_saccade_serv.shutdown()
-        self.__init__()
+        self.saccade = Saccade()
         return True
 
-def main(args):
+def main():
     rospy.init_node("saccade")
-    saccade = Saccade()
+    saccade_node = SaccadeNode()
     rospy.spin()
 
-if __name__ == '__main__':
-    main(sys.argv)
+if __name__ == "__main__":
+    main()
