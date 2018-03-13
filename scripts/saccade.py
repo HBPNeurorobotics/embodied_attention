0d4086e73865676d79ba29eeb5f3ff21a5faa03a
Fedor Scholz <scholz@fzi.de>
2018-03-13 15:39:33 +0100

    visualize saccade mechanism

--- scripts/saccade.py
+++ scripts/saccade.py
@@ -9,6 +9,7 @@ from std_msgs.msg import Float32MultiArray
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Point
 import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
 
 class SaccadeNode():
     def __init__(self):
@@ -20,10 +21,14 @@ class SaccadeNode():
 
         self.saccade_target_pub = rospy.Publisher("/saccade_target", Point, queue_size=1)
         self.saccade_potential_target_pub = rospy.Publisher("/saccade_potential_target", Point, queue_size=1)
+        self.visual_neurons_pub = rospy.Publisher("/visual_neurons", Image, queue_size=1)
+        self.motor_neurons_pub = rospy.Publisher("/motor_neurons", Image, queue_size=1)
 
         self.reset_saccade_serv = rospy.Service('/reset_saccade', ResetSaccade, self.handle_reset_saccade)
 
         self.last_time = None
+
+        self.cv_bridge = CvBridge()
 
     def saliency_map_callback(self, saliency_map):
         if self.last_time is None:
@@ -35,11 +40,23 @@ class SaccadeNode():
         lo = saliency_map.layout
         saliency_map = np.asarray(saliency_map.data[lo.data_offset:]).reshape(lo.dim[0].size, lo.dim[1].size)
 
-        (target, is_actual_target) = self.saccade.compute_saccade_target(saliency_map, dt)
+        (target, is_actual_target, V, M) = self.saccade.compute_saccade_target(saliency_map, dt)
 
         self.saccade_potential_target_pub.publish(Point(*target))
         if is_actual_target:
             self.saccade_target_pub.publish(Point(*target))
+
+        V = (V - V.min()) / (V.max() - V.min())
+        M = (M - M.min()) / (M.max() - M.min())
+
+        try:
+            visual_neurons_image = self.cv_bridge.cv2_to_imgmsg(np.uint8(V * 255.), "mono8")
+            motor_neurons_image = self.cv_bridge.cv2_to_imgmsg(np.uint8(M * 255.), "mono8")
+        except CvBridgeError as e:
+            print e
+
+        self.visual_neurons_pub.publish(visual_neurons_image)
+        self.motor_neurons_pub.publish(motor_neurons_image)
 
     def handle_reset_saccade(self, req):
         rospy.loginfo("Resetting node")
