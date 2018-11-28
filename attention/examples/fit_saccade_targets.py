76a52c36ea7e4510ed39dda04e6d81f3abda2177
Jacques KAISER <jkaiser@fzi.de>
2018-11-28 15:46:08 +0100

    better parameters

--- attention/examples/fit_saccade_targets.py
+++ attention/examples/fit_saccade_targets.py
@@ -19,7 +19,7 @@ from skimage.draw import circle
 parser = argparse.ArgumentParser(description='Test saliency model')
 parser.add_argument('--model', type=str, required=True,
                     help='path to the model.ckpt file')
-parser.add_argument('--image', type=str, required=True,
+parser.add_argument('--image', type=str, required=True, nargs='+',
                     help='path to the input image')
 parser.add_argument('--out', type=str, default='/tmp/fit_saccade_targets',
                     help='path to output folder')
@@ -37,8 +37,10 @@ except OSError as e:
 
 model_file = args.model
 saliency = Saliency(model_file=model_file)
-image = misc.imread(args.image)
-saliency_map = saliency.compute_saliency_map(image)
+images = [ misc.imread(im) for im in args.image ]
+saliency_maps = [ saliency.compute_saliency_map(im) for im in images ]
+for (i, sal) in enumerate(saliency_maps):
+    plt.imsave(path.join(args.out, 'saliency_{}.png'.format(i)), sal, cmap=plt.get_cmap('gray'))
 dt = 5.
 
 # ordered list of params to optimize
@@ -80,29 +82,37 @@ def x_to_params(param_scaling):
     return params
 
 
-def simulate(sol, n_it, i):
-    print(datetime.datetime.now())
+def simulate(sol, n_it, i, time_limit=5000):
     params = x_to_params(sol)
-    np.random.seed(args.seed)
-    saccade = Saccade(**params)
-    time = 0
-    all_times = []
-    time_limit = 1000
-    score_img = np.zeros_like(saliency_map)
-    radius_rf = 5
-    while time < time_limit:
-        (target, is_actual_target) = saccade.compute_saccade_target(saliency_map, dt=dt)
-        if is_actual_target:
-            rr, cc = circle(target[1], target[0], 5, shape=score_img.shape)
-            score_img[rr, cc] = saliency_map[rr, cc]
-            all_times.append(time)
-        time += dt
-    rate = len(all_times) * 1000. / time_limit
-    ref_rate = 3
-    # we want score to be big with few saccades
-    error = - score_img.sum() + np.exp(rate *0.5 - ref_rate)
-    print("Solution {} has rate: {}. Error: {}".format(params, rate, error))
-    return error
+    all_errors = []
+    all_rates = []
+    for sal in saliency_maps:
+        np.random.seed(args.seed)
+        saccade = Saccade(**params)
+        time = 0
+        all_times = []
+        score_img = np.zeros_like(sal)
+        radius_rf = 5
+        while time < time_limit:
+            (target, is_actual_target) = saccade.compute_saccade_target(sal, dt=dt)
+            if is_actual_target:
+                rr, cc = circle(target[1], target[0], 5, shape=score_img.shape)
+                score_img[rr, cc] = sal[rr, cc]
+                all_times.append(time)
+            time += dt
+        rate = len(all_times) * 1000. / time_limit
+        # we want score to be big with few saccades
+        if len(all_times) == 0:
+            # strongly penalize when no saccades are made
+            reg = 100
+        else:
+            reg = rate
+
+        error = - score_img.sum() + np.exp(reg *0.5)
+        all_errors.append(error)
+        all_rates.append(rate)
+    print("Solution {} has rates: {}. Error: {}".format(params, all_rates, sum(all_errors)))
+    return sum(all_errors)
 
 # get list of indices of the params we are optimizing
 optimize_params_idx = np.array([ saccade_params.keys().index(param) for param in optimize_params ])
@@ -112,7 +122,7 @@ startmean = np.ones(len(optimize_params))
 # scale bounds accordingly
 # when original param value is negative, we need to change the order of the inequality (the bounds)
 sigma0 = 0.2
-bound_factors = [0, 2.5]
+bound_factors = [0, 2.]
 bounds = [
     bound_factors[0] * np.ones(len(optimize_params)),
     bound_factors[1] * np.ones(len(optimize_params))
@@ -142,6 +152,7 @@ n_it = 0
 while not es.stop():
     solutions = es.ask()
     errors = []
+    print("Iteration {} starting at time {}".format(n_it, datetime.datetime.now()))
     for (i, sol) in enumerate(tqdm(solutions)):
         err = simulate(sol, n_it, i)
         errors.append(err)
@@ -149,6 +160,30 @@ while not es.stop():
     es.logger.add()  # write data to disc to be plotted
     es.disp()
     es.result_pretty()
-    print(es.best.x)
+    # print(es.best.x)
     #cma.plot()  # shortcut for es.logger.plot()
     n_it += 1
+
+print("Optimization finished. Data was recorded in {}".format(args.out))
+
+def plot_fitting_resuts(cmaes_path):
+    import snn_utils.visualization as viz
+    import matplotlib.pyplot as plt
+    import pickle
+    import os
+    initial = pickle.load( open( os.path.join(cmaes_path, "initial.pkl"), "rb" ) )
+    ax = viz.plot_cmaes_cost(cmaes_path)
+    ax.set_ylim(-5000, 1000)
+    plt.savefig(os.path.join(cmaes_path, 'cmaes_cost.png'))
+    ax = viz.plot_cmaes_params(cmaes_path,
+                               labels=initial['optimize_params'])
+    plt.savefig(os.path.join(cmaes_path, 'cmaes_results.png'))
+    ax.legend()
+    best, fitness = viz.get_cmaes_best(cmaes_path)
+    print("Best ever: Fitness {} solution: {}".format(fitness, best))
+
+try:
+    print("Attempting to plot results")
+    plot_fitting_resuts(args.out)
+except Exception as e:
+    print("Results could not be plotted: {}".format(e))
